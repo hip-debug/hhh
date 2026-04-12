521519227362e9cd32ce5fb7008d552aff71a3ce
hip <gameexrppro@gmail.com>
2026-04-12 19:07:51 +0300

    added product.py

--- src/models/product.py
+++ src/models/product.py
@@ -6,3 +6,7 @@ class Product:
     
     def get_total_price(self):
         return self.price * self.quantity
+    def calculate_shipping(self, weight_kg, region="default"):
+        """Рассчитывает стоимость доставки."""
+        rates = {"default": 10, "express": 30, "international": 200}
+        return rates.get(region, rates["default"]) / weight_kg
